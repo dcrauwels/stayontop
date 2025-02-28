746fbd55548ed0890a49d3d146550c696eec1f3d
dcrauwels <d.crauwels@gmail.com>
2025-02-28 21:04:53 +0100

    emailchecker now works

--- emailchecker.py
+++ emailchecker.py
@@ -1,51 +1,57 @@
 import imaplib
 import email
 import constants
+import re
 from email.header import decode_header
 
-# Gmail IMAP settings
+def checkEmail() -> str:
 
-# Connect to imap server
-try:
-    mail = imaplib.IMAP4_SSL(constants.IMAP_SERVER)
-    mail.login(constants.USERNAME, constants.PASSWORD)
-    mail.select("inbox")
-    print("IMAP login successful.")
+    # Connect to imap server
+    try:
+        mail = imaplib.IMAP4_SSL(constants.IMAP_SERVER)
+        mail.login(constants.USERNAME, constants.PASSWORD)
+        mail.select("inbox", readonly=True) # NOTE that this will keep the email unread. need to set this to False later
+        print("IMAP login successful.")
 
-except Exception as e:
-    print(f"IMAP login failed: {e}")
-    exit() #stop the rest of the script if IMAP fails.
+    except Exception as e:
+        print(f"IMAP login failed: {e}")
+        exit() #stop the rest of the script if IMAP fails.
 
 
-# Search for unread emails
-status, messages = mail.search(None, f'(UNSEEN FROM "{constants.AGENCY_ADDRESS}")')
-message_ids = messages[0].split()
+    # Search for unread emails
+    status, messages = mail.search(None, f'(UNSEEN FROM "{constants.AGENCY_ADDRESS}")')
+    message_ids = messages[0].split()
 
-for mid in message_ids:
-    # get raw mail and convert to EmailMessage class
-    _, mdata = mail.fetch(mid, "(RFC822)")
-    raw = mdata[0][1]
-    emessage = email.message_from_bytes(raw)
+    for mid in message_ids:
+        # get raw mail and convert to EmailMessage class
+        _, mdata = mail.fetch(mid, "(RFC822)")
+        raw = mdata[0][1]
+        emessage = email.message_from_bytes(raw)
 
-    # pahse
-    subject = decode_header(emessage["Subject"])[0][0]
-    if isinstance(subject, bytes):
-        subject = subject.decode()
+        # pahse
+        subject = decode_header(emessage["Subject"])[0][0]
+        if isinstance(subject, bytes):
+            subject = subject.decode()
 
-    body = ""
-    if emessage.is_multipart():
-        for part in emessage.walk():
-            if part.get_content_type() == "text/plain":
-                body = part.get_payload(decode=True).decode()
-    else:
-        body = emessage.get_payload(decode=True).decode()
+        body = ""
+        if emessage.is_multipart():
+            for part in emessage.walk():
+                if part.get_content_type() == "text/plain":
+                    body = part.get_payload(decode=True).decode()
+        else:
+            body = emessage.get_payload(decode=True).decode()
 
-    # check for property name
-    if "Herenstraat" in body: # this needs to be a regex check instead
-        property_name = "Herenstraat"
-        print(f"found {property_name}")
+        # check for property name
+        matchSubject = re.search("Nu Online: (\\w+, \\w+)", subject)
 
-mail.close()
-mail.logout()
+        if matchSubject:
+            location = matchSubject.group(1)
+            print(f"Location found: {location}")
+        else:
+            print("No location found")
+        
+
+    mail.close()
+    mail.logout()
 
 
